a03a372b583b648fe92b53a07870b12a093c32ff
Shaktidhar <sdmohapatra7@gmail.com>
2024-03-09 20:27:00 +0530

    fix oth issue

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-auth-1c4ae.firebaseapp.com',
-  projectId: 'mern-auth-1c4ae',
-  storageBucket: 'mern-auth-1c4ae.appspot.com',
-  messagingSenderId: '277641423672',
-  appId: '1:277641423672:web:2de25252aae022d51aafcd',
+  authDomain: "authentication-7427c.firebaseapp.com",
+  projectId: "authentication-7427c",
+  storageBucket: "authentication-7427c.appspot.com",
+  messagingSenderId: "977634210961",
+  appId: "1:977634210961:web:a6fd3258a9a3e3e01d7b30"
 };
 
 // Initialize Firebase
